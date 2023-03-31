fa2401cb2e162dcaed93643e56cc6aa386d5b41e
kezziamae <k_abella@logicbaseinteractive.com>
2023-03-31 16:51:57 +0800

    Added flexible blocks for popular combinations

---
+++ inc/flexible-blocks/banner.php
@@ -0,0 +1,5 @@
+<section class="banner pos-relative" style="background-image: url(<?= get_sub_field('image'); ?>);" id="banner-<?= get_row_index(); ?>">
+    <div class="container text-center pos-relative">
+        <h1 class="heading"><?= get_sub_field('heading'); ?></h1>
+    </div>
+</section>
