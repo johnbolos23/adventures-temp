f5acc915ebe899df6329c89d981af7bec746f8fd
John Ruben Bolos <81129847+johnbolos23@users.noreply.github.com>
2023-05-02 20:26:58 +0800

    Update search-results.php

--- inc/helpers/search-results.php
+++ inc/helpers/search-results.php
@@ -1,11 +1,9 @@
 <div class="col d-flex">
     <div class="text-wrapper">
-        <h6>Sample Page</h6>
-        <p> 
-            This is an example page. It’s different from a blog post because it will stay in one place and will show up in your 
-            site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. 
-            It might say something like this: Hi there! I’m a bike messenger
-        </p>
+        <h6><?= get_the_title(); ?></h6>
+        <div class="wysiwyg-content">
+			<?= wp_trim_words(get_the_content(), 55, '...'); ?>
+		</div>
     </div>
-    <button class="read-more">Read More</button>
+    <button class="read-more"><a href="<?= get_the_permalink(); ?>" style="color: inherit; text-decoration: none;">Read More</a></button>
 </div>
