9fc6eb1b467f9d0c39458f87479d8e4e345384b6
kezziamae <k_abella@logicbaseinteractive.com>
2023-05-07 21:25:57 +0800

    popular combinations responsiveness

--- inc/flexible-blocks/popular_combinations.php
+++ inc/flexible-blocks/popular_combinations.php
@@ -1,25 +1,22 @@
 <section class="page-section popular-combinations" id="popular-combinations-<?= get_row_index(); ?>">
     <div class="container">
         <div class="container-wrapper">
-            <div class="row">
-                <?php if( get_sub_field('content') ) : ?>
-                    <?php foreach( get_sub_field('content') as $item ) : ?>
-                        <div class="column">
-                            <div class="image-wrapper">
-                                <img src="<?php echo $item['first_image']; ?>" />
+            <?php if( get_sub_field('content') ) : ?>
+                <?php foreach( get_sub_field('content') as $item ) : ?>
+                    <div class="col">
+                        <div class="image-wrapper">
+                            <img src="<?php echo $item['first_image']; ?>" />
                                 <span><?= get_template_part('inc/svg/image-divider'); ?></span>
-                                <img src="<?php echo $item['second_image']; ?>" />
-                                <h4><?php echo $item['title']; ?></h4>
-                            </div>
-                          
-                            <div class="location">
-                                <span class="map-icon"><?= get_template_part('inc/svg/map-pin'); ?></span>
-                                <p class="test"><?php echo $item['location']; ?></p>
-                            </div>
+                            <img src="<?php echo $item['second_image']; ?>" />
                         </div>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </div>
+                        <h4><?php echo $item['title']; ?></h4>
+                        <div class="location">
+                            <span class="map-icon"><?= get_template_part('inc/svg/map-pin'); ?></span>
+                            <p class="test"><?php echo $item['location']; ?></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
